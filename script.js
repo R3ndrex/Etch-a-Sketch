3eca689a799a40369e38649d6d1be1b4dcd9d23b
R3ndrex <yacholohchtoli@gmail.com>
2024-09-03 18:50:51 +0300

    Create hoverRandomColor,CreateGrid,button eventlistener

---
+++ script.js
@@ -0,0 +1,38 @@
+const container = document.querySelector(".container");
+const button = document.querySelector(".change-resolution");
+createGrid(16);
+
+function hoverRandomColor(element) {
+    element.addEventListener("mouseenter", () => {
+        element.style["background-color"] = `rgb(${
+            Math.floor(Math.random() * 255) + 1
+        },${Math.floor(Math.random() * 255) + 1},${
+            Math.floor(Math.random() * 255) + 1
+        })`;
+    });
+}
+
+button.addEventListener("click", () => {
+    let amount = Number(prompt("Enter your amount of boxes", 16));
+    if (amount > 100) {
+        console.error("Too much boxes!");
+    } else {
+        while (container.lastChild) {
+            container.removeChild(container.lastChild);
+        }
+        createGrid(amount);
+    }
+});
+
+function createGrid(amount) {
+    for (let i = 0; i < amount; i++) {
+        const linecontainer = document.createElement("div");
+        container.appendChild(linecontainer);
+        for (let j = 0; j < amount; j++) {
+            const newDiv = document.createElement("div");
+            newDiv.classList.add("box");
+            hoverRandomColor(newDiv);
+            linecontainer.appendChild(newDiv);
+        }
+    }
+}
