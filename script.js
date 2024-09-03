02deb30dae4ddbe6139e249e53522dc8b6516368
R3ndrex <yacholohchtoli@gmail.com>
2024-09-03 19:23:14 +0300

    Add hoverDarkening function

--- script.js
+++ script.js
@@ -12,6 +12,26 @@ function hoverRandomColor(element) {
     });
 }
 
+function hoverDarkening(element) {
+    element.addEventListener("mouseenter", () => {
+        element.style["opacity"] = Number(element.style["opacity"]) + 0.1;
+    });
+}
+
+function createGrid(amount) {
+    for (let i = 0; i < amount; i++) {
+        const linecontainer = document.createElement("div");
+        container.appendChild(linecontainer);
+        for (let j = 0; j < amount; j++) {
+            const newDiv = document.createElement("div");
+            newDiv.classList.add("box");
+            hoverRandomColor(newDiv);
+            hoverDarkening(newDiv);
+            linecontainer.appendChild(newDiv);
+        }
+    }
+}
+
 button.addEventListener("click", () => {
     let amount = Number(prompt("Enter your amount of boxes", 16));
     if (amount > 100) {
@@ -23,16 +43,3 @@ button.addEventListener("click", () => {
         createGrid(amount);
     }
 });
-
-function createGrid(amount) {
-    for (let i = 0; i < amount; i++) {
-        const linecontainer = document.createElement("div");
-        container.appendChild(linecontainer);
-        for (let j = 0; j < amount; j++) {
-            const newDiv = document.createElement("div");
-            newDiv.classList.add("box");
-            hoverRandomColor(newDiv);
-            linecontainer.appendChild(newDiv);
-        }
-    }
-}
